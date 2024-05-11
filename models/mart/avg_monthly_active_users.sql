767c3c993a9e91cc58c00b5f1f224f7538068cbd
mnabil <supermnabil@gmail.com>
2024-05-11 13:51:25 -0400

    branch2: making a PR test2

--- models/mart/avg_monthly_active_users.sql
+++ models/mart/avg_monthly_active_users.sql
@@ -1,34 +1,34 @@
 /**
 Average MAU = Sum of DAU for the month /Number of days in the month
 **/
-WITH monthly_activities AS (
-    SELECT 
-        DATE_TRUNC('month', event_time) AS month,
-        user_id,
-        COUNT(*) AS activity_count
-    FROM 
-        {{ ref('stg_login_activity') }}
-    GROUP BY 
-        DATE_TRUNC('month', event_time),
-        user_id
-),
-monthly_active_users AS (
+    WITH monthly_activities AS (
+        SELECT 
+            DATE_TRUNC('month', event_time) AS month,
+            user_id,
+            COUNT(*) AS activity_count
+        FROM 
+            {{ ref('stg_login_activity') }}
+        GROUP BY 
+            DATE_TRUNC('month', event_time),
+            user_id
+    ),
+    monthly_active_users AS (
+        SELECT
+            DATE_TRUNC('month', event_time) AS month,
+            COUNT(DISTINCT user_id) AS active_users_count
+        FROM
+            {{ ref('stg_login_activity') }}
+        GROUP BY
+            DATE_TRUNC('month', event_time)
+    )
     SELECT
-        DATE_TRUNC('month', event_time) AS month,
-        COUNT(DISTINCT user_id) AS active_users_count
+        ma.month as activity_month,
+        AVG(ma.activity_count) AS avg_activities_per_user
     FROM
-        {{ ref('stg_login_activity') }}
+        monthly_activities ma
+    JOIN
+        monthly_active_users mau ON ma.month = mau.month
     GROUP BY
-        DATE_TRUNC('month', event_time)
-)
-SELECT
-    ma.month as activity_month,
-    AVG(ma.activity_count) AS avg_activities_per_user
-FROM
-    monthly_activities ma
-JOIN
-    monthly_active_users mau ON ma.month = mau.month
-GROUP BY
-    ma.month
-ORDER BY
-    ma.month
+        ma.month
+    ORDER BY
+        ma.month
